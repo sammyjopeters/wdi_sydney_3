38c40e148c6f3009d6b9bf7687701bcf984357e2
lukemesiti <lukemesiti@gmail.com>
2014-01-12 12:58:42 +1100

    phase 2 of w1d5 hw (animal shelter) complete

--- students/luke_mesiti/w1d5/main.rb
+++ students/luke_mesiti/w1d5/main.rb
@@ -1,4 +1,4 @@
-# require './shelter'
+require './shelter'
 require './client'
 require './animal-prompt'
 
@@ -9,10 +9,25 @@ require './animal-prompt'
 
 # def initialize(name, number_of_children, age, number_of_pets, gender)
 client = Client.new("Beth", 3, 30, 1, "female")
+client2 = Client.new("John", 1, 45, 14, "male")
 
 # def initialize(name, age, gender, species, toys)
 animal = Animal.new("Tiny", 3, "male", "cat", "string")
+animal2 = Animal.new("Biggy", 2, "female", "dog", "frisbee")
 
-puts client.to_s
+# def initialize(name, address, animal, client)
+shelter = Shelter.new("HappiTails", "East 21st Street", animal, client)
+shelter.add_animal(animal2)
+shelter.add_client(client2)
 
-puts animal.to_s
+
+# puts client.to_s
+
+# puts animal.to_s
+
+# puts shelter.to_s
+puts "shelter.shelter_animals: "
+puts shelter.shelter_animals
+
+puts "shelter.shelter_clients: "
+puts shelter.shelter_clients
--- students/luke_mesiti/w1d5/shelter.rb
+++ students/luke_mesiti/w1d5/shelter.rb
@@ -1,19 +1,28 @@
-require './animal-prompt'
-require './client'
+# require './animal-prompt'
+# require './client'
 
 class Shelter
 
-	attr_accessor :name, :animal, :client
+	attr_accessor :name, :address, :shelter_animals, :shelter_clients
 
-	def initialize(animal, client)
-		@animal = animal
-		@client = client
+	def initialize(name, address, animal, client)
+		@name = name
+		@address = address
+		@shelter_animals = [animal]
+		@shelter_clients = [client]
 	end
 
 	def to_s
-		""
+		"#{name} shelter at #{address} has " 
+	#	#{animal} animals and #{client} clients."
+	end
 
+	def add_animal(animal)
+		shelter_animals.push(animal)
+	end
 
-
+	def add_client(client)
+		shelter_clients.push(client)
+	end
 
 end
