da8160f2f6653d7a90329cfda5fa997a28930dcf
sammyjopeters <peters.sammyjo@gmail.com>
2014-01-20 23:23:25 +1100

    2014-01-20 Homework update: Basic spec complete

--- students/samantha_peters/week3/week3day1/rogbloll/main.rb
+++ students/samantha_peters/week3/week3day1/rogbloll/main.rb
@@ -4,14 +4,42 @@ require 'sinatra/reloader' if development?
 require 'active_support/all'
 require 'pg'
 
-
+#display the homepage
 get '/' do 
 
+  #get SQL to display the info from the posts table in the database
   sql = "SELECT * FROM posts"
+  #whatever result comes from running the SQL, make it into the @post variable
   @post = run_sql(sql)
+  #allow us to replace @id with the id that comes from the SQL...
   @id = params[:id]
 
   erb :home  
+end
+
+get '/post/:id' do
+
+  sql = "SELECT * FROM posts WHERE id=#{params[:id]}"
+  @records = run_sql(sql)
+  @post = @records[0]
+
+erb :viewpost
+end
+
+
+#get the 'create a post' page!
+
+get '/newpost' do
+  erb :creatpost
+end
+
+
+#the action for creating a post
+post '/create' do
+  sql = "INSERT INTO posts (title, tagline, postbody, author, created) VALUES ('#{params[:title]}', '#{params[:tagline]}', '#{params[:postbody]}', '#{params[:author]}', '#{Time.now}')"
+  #run the query
+  run_sql(sql)
+  redirect to '/'
 end
 
 
