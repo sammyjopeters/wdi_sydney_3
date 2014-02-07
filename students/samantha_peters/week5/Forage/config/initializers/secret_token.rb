c5696a6c824938d297ee5456f7a9aac5293d60f1
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-07 16:44:37 +1100

    I cant remember what ive done recently

---
+++ students/samantha_peters/week5/Forage/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forage::Application.config.secret_key_base = '5212709a0928f7f8d0a793f1df48044326ce99e901080a0944c713932408b1868e797ae850fd55c9faa9bfdc00405b777bf446963168fc4875650beb88bc81b7'
