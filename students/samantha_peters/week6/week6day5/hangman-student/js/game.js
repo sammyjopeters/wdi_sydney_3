f93a232257cc8d15dcf7cc4bc83f7cba579ef258
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-14 17:12:59 +1100

    Stage 1 hangman homework (week6day5)

--- students/samantha_peters/week6/week6day5/hangman-student/js/game.js
+++ students/samantha_peters/week6/week6day5/hangman-student/js/game.js
@@ -1,22 +1,31 @@
 var word = {
   secretWord: "",
-  wordList: ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'],
+  wordList: ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database', 'amazeballs', 'barbeque', 'machete', 'honorificabilitudinitatibus', 'floccinaucinihilipilification'],
 
   // Selects a random word from the word list sets the secret word
-  setSecretWord: function(){},
+  setSecretWord: function(){
+    this.secretWord = _.sample(this.wordList, [1])  //this.wordList[Math.floor(Math.random() * this.wordList.length)];
+  },
 
   // Takes an array of letters as input and returns an array of two items:
   // 1) A string with the parts of the secret word that have been guessed correctly and underscore for the parts that haven't
   // 2) An array of all the guessed letters that were not in the secret word
-  checkLetters: function(guessedLetters){}
+  checkLetters: function(guessedLetters){
+    var wordProgress = [];
+    var totalMisses = [];
+  }
 };
 
 var player = {
   MAX_GUESSES: 8,
   guessedLetters: [],
 
-  // Takes a new letter as input and updates the game
-  makeGuess: function(letter){},
+
+  // Takes the clean letter from checkInput() and adds to the current list of guesses
+  makeGuess: function(letter){
+    this.guessedLetters.push(letter);
+  },
+    
 
   // Check if the player has won and end the game if so
   checkWin: function(wordString){},
@@ -36,9 +45,55 @@ var game = {
   updateDisplay: function(secretWordWithBlanks, guessedLetters, guessesLeft){}
 };
 
+
+function checkInput(input){
+  currentGuess = "";
+
+
+    //if the value of the box is one character
+    if (input.length === 1){
+        // now check that it's only got characters, no number or symbols...
+        if (input.match(/^a-z/)) {
+          updateError("symbols");
+        }else{
+          currentGuess = input;
+          player.makeGuess(currentGuess);
+        }
+    } else {
+      //there's more than one character here. freak out!
+      updateError("tooManyChars");
+    }   
+
+}
+
+function updateError(errorKey){
+  var errorDiv = document.getElementById('errors');
+  if (errorKey === "tooManyChars"){
+  errorDiv.innerHTML = "You can only enter one character!"
+  } else if (errorKey === "symbols"){
+  errorDiv.innerHTML = "Numbers and symbols not allowed!"
+  }
+}
+
 window.onload = function(){
+  //declare the variables you get off the HTML here, KTHX
+  var resetButton = document.getElementById('resetButton');
+  var giveUpButton = document.getElementById('resetButton');
+  var letterField = document.getElementById('letterField');
+
+
+
   // Start a new game
+  word.setSecretWord();
   // Add event listener to the letter input field to grab letters that are guessed
+  //only execute the function if enter is pressed, though!
+  letterField.addEventListener('keyup', function(e) {
+    if (e.keyCode === 13){
+    checkInput(letterField.value);
+    }
+  });
   // Add event listener to the reset button to reset the game when clicked
+  giveUpButton.addEventListener('click', game.resetGame);
   // Add event listener to the give up button to give up when clicked
+  giveUpButton.addEventListener('click', game.giveUp);
 };
